f226a2c5b9d794e8db44d6b63fe1a6e4e0d41910
GardoKirsipuu <gardo1999@gmail.com>
2022-02-08 09:24:53 +0200

    Displayed all articles

--- index.js
+++ index.js
@@ -12,12 +12,11 @@ app.engine('hbs', hbs.engine({
 	extname: 'hbs',
 	defaultLayout: 'main',
 	layoutsDir: __dirname + '/views/layouts/'
-}))
+}));
 // setup static public directory
 app.use(express.static('public'));
 
-
-const mysql = require('mysql2')
+const mysql = require('mysql')
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended: true}))
@@ -28,14 +27,27 @@ var con = mysql.createConnection({
 	user: "root",
 	password: "qwerty",
 	database: "yoga_mysql"
-})
+});
 
 con.connect(function(err) {
 	if (err) throw err;
 	console.log("Connected to yoga_mysql db");
-})
+});
+
+// show all articles - index page
+app.get('/', (req, res) => {
+	let query = "SELECT * FROM article";
+	let qrticles = []
+	con.query(query, (err, result) => {
+		if (err) throw err;
+		articles = result
+		res.render('index', {
+			articles: articles
+		})
+	})
+});
 
 // app start point
 app.listen(3000, () => {
 	console.log('App is started at http://localhost:3000')
-})
+});
